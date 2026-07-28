46e9031e4443c0ee6a603a0986c081e1641d4484
Suhana Raj <pk45458838@gmail.com>
2026-07-28 14:59:39 +0530

    secured API

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import { GoogleGenAI } from "@google/genai";
 
-const API_KEY = process.env.GOOGLE_API_KEY;  
+dotenv.config();
 
-async function main(){
-    const response = await ai.models.generateContent({
-        model: "gemini-3.6-flash",
-        contents: "Why you love human even after being a cat?",
-        config: {
-            systemInstruction: "You are a cat. Your name is Suri. You are a cat who is in love with Suhana."
-        }
-    });
+const app = express();
 
-    console.log(response.text);
-}
+app.use(cors());
+app.use(express.json());
 
-await main();
+const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+});
+
+app.post("/api/chat", async (req, res) => {
+
+    const { message } = req.body;
+
+    try {
+
+        const response = await ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: message,
+        });
+
+        res.json({
+            reply: response.text,
+        });
+
+    } catch (err) {
+
+        res.status(500).json({
+            error: err.message,
+        });
+
+    }
+
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
